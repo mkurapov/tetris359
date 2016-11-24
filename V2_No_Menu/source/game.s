b72c975751da0bf4776e3df2fa360c817b4d2f34
max.kurapov <max.kurapov@pi03-eb.cs.ucalgary.ca>
2016-11-24 12:50:03 -0700

    final merged changes

--- V2_No_Menu/source/game.s
+++ V2_No_Menu/source/game.s
@@ -8,16 +8,16 @@
 Game_Display:
 
 	push	{r4-r10, r14}
-	
+
 	ldr		r0, =Game_Screen
 	bl		Draw_Image
 	bl		Score_Draw
-	
+
 	pop		{r4-r10, r14}
 	bx		lr
-	
-	
-	
+
+
+
 /* Game_Test function
  *	global function to test implementation of individual functions within the game package
  */
@@ -25,41 +25,41 @@ Game_Display:
 Game_Test:
 
 	push	{r4-r10, r14}
-	
+
 	bl 		Game_Reset
-	
+
 	bl		Game_Display
 	bl		Toggle_Game_Running_Flag
-	
-	
+
+
 	bl		Value_Pack_Spawn
-	
+
 	//bl		Value_Pack_Draw
 	//bl		Value_Pack_Clear
-	
+
 	testing:
 	//test spawning
 	bl		Spawn_Tetromino
 	cmp		r0, #0
 	bleq	Quit
-	
+
 		moveIt:
 			ldr		r1, =Delay
 			ldr		r0, [r1]
 			bl		User_Turn
-			
+
 			bl		Move_Down
-			
+
 			mov		r4, r0
 			cmp		r4, #0
 			bleq	Clear_Rows
 			cmp		r4, #0
 			beq		testing
-			
+
 			b		moveIt
-	
+
 	endGameTest:
-	
+
 	pop		{r4-r10, r14}
 	bx		lr
 
@@ -81,53 +81,69 @@ User_Turn:
 	maskDo	.req r9
 	maskSt	.req r10
 
-	push	{r4-r10, r14}				
+	push	{r4-r10, r14}
 
 	//add offset to current time to for loop guard
-	ldr		timReg, =0x3F003004			
-	ldr		basTim, [timReg]			
-	add		reqTim, basTim, r0		
-	
+	ldr		timReg, =0x3F003004
+	ldr		basTim, [timReg]
+	add		reqTim, basTim, r0
+
 	//setup bit masks
 	mov		r0, #1
+
+	mov		maskSt, #8
+	mov		maskUp, #16
+	mov		maskDo, #32
 	mov		maskLe, #64
 	mov		maskRi, #128
-	mov		maskSt, #8
+
 
 	//loop until the current time == required time
-	delayLoop:	
-	
+	delayLoop:
+
 		//read in the SNES input
 		bl		Get_SNES
-		
+
 		//check if left was pressed, and attempt move
 		and		r1, maskLe, button
 		cmp		r1, #0
 		blne	Move_Left
-		
+
 		//check if right was pressed, and attempt move
-		and		r1, maskRi, button 
+		and		r1, maskRi, button
 		cmp		r1, #0
 		blne	Move_Right
-		
+
+		//check if right was pressed, and attempt move
+		and		r1, maskUp, button
+		cmp		r1, #0
+		movne	r0, #0
+		blne	Rotate_Tet
+
+		//check if right was pressed, and attempt move
+		and		r1, maskDo, button
+		cmp		r1, #0
+		movne	r0, #1
+		blne	Rotate_Tet
+
 		//check if start was pressed, and display the pause menu if so
-		and		r1, maskSt, button 
+		and		r1, maskSt, button
 		cmp		r1, #0
 		blne	Pause_Run
-		
-	
+
+
 	//***********************************************
 	//	bl		Interrupt_Reinstall_Table
 	//***********************************************
-		
-		
-		
-		
+
+
+
+
 		//loop until delay is reached
-		ldr		curTim, [timReg]		
-		cmp		curTim, reqTim			
+		ldr		curTim, [timReg]
+		cmp		curTim, reqTim
 		blo		delayLoop
-				
+
 	endDelayLoop:
 
 	.unreq	button
@@ -141,7 +157,7 @@ User_Turn:
 	.unreq	maskDo
 	.unreq	maskSt
 
-	
+
 	pop		{r4-r10, r14}				//pop registers off of the stack
 	bx		lr							//branch back to the calling subroutine
 
@@ -172,7 +188,7 @@ Set_Tetromino:
 	//get the base address for the coordinates and the virtual board
 	ldr		tetAdd, =First
 	ldr		boaAdd, =Virtual_Board
-	
+
 	//retrieve the type of the current tetromino
 	ldr		type, =Type
 	ldrb	type, [type]
@@ -182,21 +198,21 @@ Set_Tetromino:
 	mov		length, #12
 
 	setLoop:
-	
+
 		//check loop guard
 		cmp		index, #4
 		bge		endSetLoop
 
-		//get the (x,y) coords 
+		//get the (x,y) coords
 		ldrb	xCoord, [tetAdd], #1
 		ldrb	yCoord, [tetAdd], #1
 		mul		yCoord, length
-		
+
 		//calculate the offset and set the cell
 		add		celAdd, xCoord, yCoord
 		add		celAdd, boaAdd
 		strb	type, [celAdd]
-		
+
 		//incerement loop counter
 		add		index, #1
 		b		setLoop
@@ -211,7 +227,7 @@ Set_Tetromino:
 	.unreq	index
 	.unreq	type
 	.unreq	length
-	
+
 	pop		{r4-r10, r14}
 	bx		lr
 
@@ -244,21 +260,21 @@ Clear_Tetromino:
 	mov		length, #12
 
 	clearLoop:
-	
+
 		//check loop guard
 		cmp		index, #4
 		bge		endClearLoop
 
-		//get the (x,y) coords 
+		//get the (x,y) coords
 		ldrb	xCoord, [tetAdd], #1
 		ldrb	yCoord, [tetAdd], #1
 		mul		yCoord, length
-		
+
 		//calculate the offset and clear the cell
 		add		celAdd, xCoord, yCoord
 		add		celAdd, boaAdd
 		strb	clear, [celAdd]
-		
+
 		//incerement loop counter
 		add		index, #1
 		b		clearLoop
@@ -273,7 +289,7 @@ Clear_Tetromino:
 	.unreq	index
 	.unreq	clear
 	.unreq	length
-	
+
 	pop		{r4-r10, r14}
 	bx		lr
 
@@ -285,58 +301,58 @@ Clear_Tetromino:
 Set_Tet_Screen:
 
 	push	{r4-r10, r14}
-	
+
 	type	.req r4
 	tetAdd	.req r5
 	xCoord	.req r6
 	yCoord	.req r7
 	typAdd	.req r8
 	index	.req r9
-	
+
 	//get the type of the current tetromino, and get address of block image
 	ldr		typAdd, =Type
 	ldrb	r0, [typAdd]
 	bl		ASCII_To_Address
 	mov		type, r0
-	
+
 	//set up index
 	mov		index, #0
 	ldr		tetAdd, =First
-	
+
 	setTetLoop:
-	
+
 		//check loop guard
 		cmp		index, #4
 		bge		endSetTetLoop
-	
+
 		//get x and y coordinates
 		ldrb	xCoord, [tetAdd], #1
 		ldrb	yCoord, [tetAdd], #1
-		
+
 		//call draw block
 		mov		r0, type
 		mov		r1, xCoord
 		mov		r2, yCoord
 		bl		Draw_Block
-		
+
 		//increment index
 		add		index, #1
 		b		setTetLoop
-	
+
 	endSetTetLoop:
-	
+
 	.unreq	type
 	.unreq	tetAdd
 	.unreq	xCoord
 	.unreq	yCoord
 	.unreq	typAdd
 	.unreq	index
-	
+
 	pop		{r4-r10, r14}
 	bx		lr
-	
-	
-	
+
+
+
 /*Clear_Tet_Screen function
  * Draw out just Tetromino block to the display
  */
@@ -344,52 +360,52 @@ Set_Tet_Screen:
 Clear_Tet_Screen:
 
 	push	{r4-r10, r14}
-	
+
 	type	.req r4
 	tetAdd	.req r5
 	xCoord	.req r6
 	yCoord	.req r7
 	index	.req r8
-	
+
 	//set the empty block character for the type
 	mov		r0, #'E'
 	bl		ASCII_To_Address
 	mov		type, r0
-	
+
 	//set up index
 	mov		index, #0
 	ldr		tetAdd, =First
-	
+
 	clearTetLoop:
-	
+
 		//check loop guard
 		cmp		index, #4
 		bge		endClearTetLoop
-	
+
 		//get x and y coordinates
 		ldrb	xCoord, [tetAdd], #1
 		ldrb	yCoord, [tetAdd], #1
-		
+
 		//call draw block
 		mov		r0, type
 		mov		r1, xCoord
 		mov		r2, yCoord
 		bl		Draw_Block
-		
+
 		//increment index
 		add		index, #1
 		b		clearTetLoop
-	
+
 	endClearTetLoop:
-	
+
 	.unreq	type
 	.unreq	tetAdd
 	.unreq	xCoord
 	.unreq	yCoord
 	.unreq	index
-	
+
 	pop		{r4-r10, r14}
-	bx		lr	
+	bx		lr
 
 
 
@@ -417,45 +433,45 @@ Redraw_Board:
 	mov		yCoord, #0
 	mov		outInd,	#0
 	mov		innInd, #0
-	
+
 	redrawOuterLoop:
-	
+
 		//check loop guard
 		cmp		yCoord, #22
 		bge		endRedrawOuterLoop
-	
+
 		redrawInnerLoop:
-		
+
 			//check loop guard
 			cmp		xCoord, #12
 			bge		endRedrawInnerLoop
-		
+
 			//get the address from ascii for each block
 			ldrb	block, [offset], #1
-			
+
 			//skip until the 2rd line
 			cmp		yCoord, #1
 			ble		incRedrawInnerLoop
-			
+
 			bl		ASCII_To_Address
-			
+
 			//draw the block at the corresponding spot on the screen
 			mov		r1, xCoord
 			mov		r2, yCoord
 			bl		Draw_Block
-		
+
 			incRedrawInnerLoop:
 			//increment the x coordinate
 			add		xCoord, #1
 			b		redrawInnerLoop
-		
+
 		endRedrawInnerLoop:
-		
+
 		//increment the y coordinate and reset x
 		add		yCoord, #1
 		mov		xCoord, #0
 		b		redrawOuterLoop
-	
+
 	endRedrawOuterLoop:
 
 	.unreq	offset
@@ -478,7 +494,7 @@ Redraw_Board:
  *	r0 - x coordinate
  *	r1 - y coordinate
  *	Returns:
- *		r0- 1 if empty, 0 otherwise	
+ *		r0- 1 if empty, 0 otherwise
  */
 .global Check_Empty
 Check_Empty:
@@ -488,7 +504,7 @@ Check_Empty:
 	offset	.req r2
 	block	.req r3
 	length	.req r4
-	
+
 
 	push	{r4-r10, r14}
 
@@ -498,17 +514,17 @@ Check_Empty:
 	mul		yCoord, length
 	add		offset, xCoord
 	add		offset, yCoord
-	
+
 	//get the ascii value from the virtual board and check its value
 	ldrb	block, [offset]
 	cmp		block, #'E'
 	moveq	r0, #1
 	movne	r0, #0
 
-	.unreq	xCoord	
-	.unreq	yCoord	
-	.unreq	offset	
-	.unreq	block	
+	.unreq	xCoord
+	.unreq	yCoord
+	.unreq	offset
+	.unreq	block
 	.unreq	length
 
 	pop		{r4-r10, r14}
@@ -530,33 +546,33 @@ Game_Reset:
 
 	//set up the index to clear the game board
 	mov		index, #0
-	
+
 	clearBoardLoop:
-	
+
 		//check the loop guard
 		cmp		index, #21
 		bge		endClearBoardLoop
-		
+
 		//clear the row corresponding to the current index
 		mov		r0, index
 		bl		Clear_Row
-	
+
 		//increment the index
 		add		index, #1
 		b		clearBoardLoop
-	
+
 	endClearBoardLoop:
-	
+
 	//reset the score
 	ldr 	r0, =Score
 	mov		r1, #0
 	strb	r1, [r0]
-	
+
 	//reset the user turn delay in case the game was quit while a value pack was active
 	ldr		r0, =Delay
 	mov		r1, #100
 	strb	r1, [r0]
-	
+
 	//reset the default tetromino spawn
 	ldr		r0, =First
 	mov		r1, #4
@@ -575,12 +591,12 @@ Game_Reset:
 	strb	r1, [r0], #1
 	mov		r1, #1
 	strb	r1, [r0]
-	
+
 	//reset the tetromino type to the default values
 	ldr		r0, =Type
 	mov		r1, #'C'
-	strb	r1, [r0]	
-	
+	strb	r1, [r0]
+
 	//reset the tetromino orientation to the default value
 	ldr		r0, =Orientation
 	mov		r1, #'u'
@@ -606,7 +622,7 @@ Toggle_Game_Running_Flag:
 	//get the game running flag
 	ldr		r0, =Game_Running_Flag
 	ldr		r1, [r0]
-	
+
 	//check the contents, and toggle the flag
 	cmp		r1, #0
 	streqb	r3, [r0]
--- V2_No_Menu/source/utilities.s
+++ V2_No_Menu/source/utilities.s
@@ -42,177 +42,6 @@ Wait:
 
 
 
-
-
-
-/* Random_Number_Generator function
-* Generates a random number from 0 to 3
-* Parameters:
-*	None
-*Returns:
-*	r0 - rand(0,3)
-*/
-.global		Random_Number_Generator
-Random_Number_Generator:
-
-clock	  		.req r0
-clockReg		.req r1
-shiftClock	.req r2
-mask				.req r3
-
-push			{r4-r10, r14}				//push registers onto the stack
-
-//set up loop
-ldr		clockReg, =0x3F003004			//get the address for the time counter
-ldr		clock, [clockReg]			//get the base time from the time address
-
-//lsr		shiftClock, clock, #12
-eor 	clock, clock, lsr #12
-eor 	clock, clock, lsl #25
-eor 	clock, clock, lsr #27
-
-
-
-.unreq	clockReg
-.unreq	clock
-.unreq	shiftClock
-.unreq	mask
-
-pop		{r4-r10, r14}				//pop registers off of the stack
-bx		lr							//branch back to the calling subroutine
-
-
-
-
-
-
-/* Get_Random_Tetromino function
- * Generates a random number from 0 to 6
- * Parameters:
- *	None
- *Returns:
- *	r0 - rand(0,6)
- */
-.global	Get_Random_Tetromino
-Get_Random_Tetromino:
-
-	result	  	.req r0
-	mask				.req r4
-	rand				.req r5
-
-
-	push			{r4-r10, r14}				//push registers onto the stack
-
-	mov	mask, #15
-
-	bl	Random_Number_Generator
-
-	and	result, mask
-
-	cmp	result, #13
-	subhi	result, #9
-	bhi		endGetRandomTet
-
-	cmp	result, #6
-	subhi	result, #7
-	bhi		endGetRandomTet
-
-	endGetRandomTet:
-
-	.unreq	result
-	.unreq	mask
-	.unreq	rand
-
-	pop		{r4-r10, r14}				//pop registers off of the stack
-	bx		lr							//branch back to the calling subroutine
-
-
-
-
-
-
-/* Get_Random_Row function
- * Generates a random number from 0 to 18
- * Parameters:
- *	None
- *Returns:
- *	r0 - rand(0,18)
- */
-.global	Get_Random_Y
-Get_Random_Y:
-
-	result	  	.req r4
-	i						.req r5
-
-
-	push			{r4-r10, r14}				//push registers onto the stack
-
-	mov	i, #0
-
-	//loop random number generator 6 times, max 18
-	getRandomRowLoop:
-			cmp	i, #6
-			beq	endRandomRow
-
-			bl	Random_Number_Generator
-			add	result, r0
-
-			add	i, #1
-			b	getRandomRowLoop
-
-	endRandomRow:
-
-	mov	r0, result
-
-	.unreq	result
-	.unreq	i
-
-	pop		{r4-r10, r14}				//pop registers off of the stack
-	bx		lr							//branch back to the calling subroutine
-
-
-/* Get_Random_X function
- * Generates a random number from 0 to 9
- * Parameters:
- *	None
- *Returns:
- *	r0 - rand(0,9)
- */
-.global	Get_Random_X
-Get_Random_X:
-
-	result	  	.req r4
-	i						.req r5
-
-
-	push			{r4-r10, r14}				//push registers onto the stack
-
-	mov	i, #0
-
-	//loop random number generator 3 times, max 9
-	getRandomColumnLoop:
-			cmp	i, #3
-			beq	endRandomColumn
-
-			bl	Random_Number_Generator
-			add	result, r0
-
-			add	i, #1
-			b	getRandomColumnLoop
-
-	endRandomColumn:
-
-	mov	r0, result
-
-	.unreq	result
-	.unreq	i
-
-	pop		{r4-r10, r14}				//pop registers off of the stack
-	bx		lr							//branch back to the calling subroutine
-
-
-
-
 /* ASCII_To_Address function
  *	converts an ASCII character to the base address of the associated block type
  *	r0 - ASCII character
@@ -260,11 +89,11 @@ ASCII_To_Address:
 	cmp		r0, #'R'
 	ldreq	r0, =Red_Block
 	beq		end_ASCII_To_Address
-	
+
 	cmp		r0, #'F'
 	ldreq	r0, =Value_Pack_Fast
 	beq		end_ASCII_To_Address
-	
+
 	cmp		r0, #'I'
 	ldreq	r0, =Value_Pack_Invisible
 	beq		end_ASCII_To_Address
@@ -272,9 +101,9 @@ ASCII_To_Address:
 	end_ASCII_To_Address:
 	pop		{r4-r10, r14}
 	bx		lr
-	
-	
-	
+
+
+
 /* Digit_To_Address function
  *	converts an digit to the base address of the associated number
  *	r0 - digit
@@ -322,7 +151,7 @@ Digit_To_Address:
 	cmp		r0, #8
 	ldreq	r0, =Char_8
 	beq		end_Digit_To_Address
-	
+
 	cmp		r0, #9
 	ldreq	r0, =Char_9
 	beq		end_Digit_To_Address
@@ -330,9 +159,9 @@ Digit_To_Address:
 	end_Digit_To_Address:
 	pop		{r4-r10, r14}
 	bx		lr
-	
-	
-	
+
+
+
 
 
 /* Number_To_Bit function
@@ -355,41 +184,41 @@ Number_To_Bit:
 	mov		mask, #1
 
 	numberToBitLoop:
-	
+
 		//check loop guard
 		cmp		index, #32
 		bge		endNumberToBitLoop
-		
+
 		//get the state of the current bit
 		and		bit, mask, r0
-	
+
 		//check if the bit is set, and exit if so
 		cmp		bit, #0
 		bne		endNumberToBitLoop
-		
+
 		//otherwise, increment the index and shift the mask
 		add		index, #1
 		lsl		mask, #1
 		b		numberToBitLoop
-	
+
 	endNumberToBitLoop:
-	
+
 	//if not bits were found, return 0
 	cmp		index, #32
 	movge		index, #0
-	
+
 	//move the index out to the result
 	mov		r0, index
-	
+
 	.unreq	index
 	.unreq	mask
 	.unreq	bit
-	
+
 	pop		{r4-r10, r14}
 	bx		lr
-	
-	
-	
+
+
+
 
 /* Isolate_Digits function
  *	isolates the digit of the hundreds, tens, and ones spot of a number
@@ -413,39 +242,39 @@ Isolate_Digits:
 	mov		index, #0
 
 
-	hundredsLoop:	
-	
+	hundredsLoop:
+
 		//subtract 100 until number <100 to count how many hundreds compose the number
-		cmp 	number, #100					
-		subhs 	number, #100					
-		addhs	index, #1					
-		bhs 	hundredsLoop	
-	
+		cmp 	number, #100
+		subhs 	number, #100
+		addhs	index, #1
+		bhs 	hundredsLoop
+
 	endHundredsLoop:
-	
+
 	//move the hundreds digit to the return register and reset the index
 	mov		r0, index
 	mov 	index, #0
-	
+
 	tensLoop:
-	
+
 		//subtract 10 until number <10 to count how many tens compose the number
-		cmp number, #10	
-		subhs number, #10	
-		addhs index, #1		
-		bhs tensLoop		
-	
+		cmp number, #10
+		subhs number, #10
+		addhs index, #1
+		bhs tensLoop
+
 	endTensLoop:
-	
+
 	//move the tens and ones digits to the return registers
 	mov		r1, index
-	mov		r2, number 
-	
+	mov		r2, number
+
 	.unreq	number
 	.unreq	index
-		
+
 	pop		{r4-r10, r14}
 	bx		lr
-	
+
 
 .section .data
